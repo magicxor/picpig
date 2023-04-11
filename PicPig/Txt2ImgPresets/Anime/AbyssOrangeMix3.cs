46e825c91b3d5d62c04663625ffaaa6d0309e3cc
Ilia Burakov <burakov.ilia@gmail.com>
2023-04-11 19:55:41 +0800

    fix AbyssOrangeMix3

--- PicPig/Txt2ImgPresets/Anime/AbyssOrangeMix3.cs
+++ PicPig/Txt2ImgPresets/Anime/AbyssOrangeMix3.cs
@@ -5,9 +5,9 @@ namespace PicPig.Txt2ImgPresets.Anime;
 // AbyssOrangeMix3 (AOM3): https://civitai.com/models/9942/abyssorangemix3-aom3
 public class AbyssOrangeMix3 : BasePresetFactory
 {
-    public override string DefaultPositivePrompt => @"masterpiece, best quality, ultra-detailed, illustration, 1girl, solo, fantasy, flying, broom, night sky, outdoors, magic, spells, moon, stars, clouds, wind, hair, cape, hat, boots, broomstick, glowing, mysterious, enchanting, whimsical, playful, adventurous, freedom, wonder, imagination, determination, skill, speed, movement, energy, realism, naturalistic, figurative, representational, beauty, fantasy culture, mythology, fairy tales, folklore, legends, witches, wizards, magical creatures, fantasy worlds, composition, scale, foreground, middle ground, background, perspective, light, color, texture, detail, beauty, wonder";
+    public override string DefaultPositivePrompt => @"masterpiece, best quality, ultra-detailed, illustration, 1girl, solo, outdoors, camping, night, mountains, nature, stars, moon, bonfire, tent, twin ponytails, green eyes, cheerful, happy, backpack, sleeping bag, camping stove, water bottle, mountain boots, gloves, sweater, hat, flashlight, forest, rocks, river, wood, smoke, shadows, contrast, clear sky, constellations, Milky Way, peaceful, serene, quiet, tranquil, remote, secluded, adventurous, exploration, escape, independence, survival, resourcefulness, challenge, perseverance, stamina, endurance, observation, intuition, adaptability, creativity, imagination, artistry, inspiration, beauty, awe, wonder, gratitude, appreciation, relaxation, enjoyment, rejuvenation, mindfulness, awareness, connection, harmony, balance, texture, detail, realism, depth, perspective, composition, color, light, shadow, reflection, refraction, tone, contrast, foreground, middle ground, background, naturalistic, figurative, representational, impressionistic, expressionistic, abstract, innovative, experimental, unique";
 
-    public override string DefaultNegativePrompt => @"(worst quality, low quality:1.4), (lip, nose, tooth, rouge, lipstick, eyeshadow:1.4), (jpeg artifacts:1.4), (depth of field, bokeh, blurry, film grain, chromatic aberration, lens flare:1.0), greyscale, monochrome, dusty sunbeams, trembling, motion lines, motion blur, emphasis lines, text, title, logo, signature";
+    public override string DefaultNegativePrompt => @"(worst quality, low quality:1.4), (realistic, lip, nose, tooth, rouge, lipstick, eyeshadow:1.0), (dusty sunbeams:1.0),, (abs, muscular, rib:1.0), (depth of field, bokeh, blurry:1.4), (greyscale, monochrome:1.0), text, title, logo, signature";
 
     public override StableDiffusionProcessingTxt2Img GetRequestData(string? positivePrompt, int samplingSteps = DefaultSamplingSteps)
     {
@@ -15,7 +15,7 @@ public class AbyssOrangeMix3 : BasePresetFactory
         {
             Override_settings = new
             {
-                sd_model_checkpoint = "abyssorangemix3AOM3_aom3a1b.safetensors",
+                sd_model_checkpoint = "abyssorangemix3AOM3_aom3a3.safetensors",
                 eta_noise_seed_delta = 31337,
                 CLIP_stop_at_last_layers = 2,
                 // maybe orangemix.vae.pt is better?
@@ -33,16 +33,16 @@ public class AbyssOrangeMix3 : BasePresetFactory
             Sampler_name = "DPM++ SDE Karras",
             Batch_size = 1,
             N_iter = 1,
-            Steps = samplingSteps,
+            Steps = 12,
             Cfg_scale = 7,
-            Width = 512,
-            Height = 832,
+            Width = 768,
+            Height = 512,
             Restore_faces = false,
             Tiling = false,
             Do_not_save_samples = false,
             Do_not_save_grid = true,
             Eta = null,
-            Denoising_strength = 0.98,
+            Denoising_strength = 0.5,
             S_churn = 0.0,
             S_tmax = null,
             S_tmin = 0.0,
@@ -51,8 +51,8 @@ public class AbyssOrangeMix3 : BasePresetFactory
             Save_images = true,
 
             Enable_hr = true,
-            Hr_scale = 1.3,
-            Hr_upscaler = "R-ESRGAN 4x+ Anime6B",
+            Hr_scale = 1.5,
+            Hr_upscaler = "SwinIR_4x",
             Hr_second_pass_steps = 7,
         };
     }
